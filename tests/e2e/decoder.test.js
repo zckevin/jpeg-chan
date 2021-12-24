3b41b0a1e662e420a9694837513c319b5c41f973
Cong Zuo <zckevinzc@gmail.com>
2021-12-24 09:28:37 +0800

    Update tests.

--- tests/e2e/decoder.test.js
+++ tests/e2e/decoder.test.js
@@ -24,26 +24,28 @@ describe("Check JPEG decoders", () => {
       expect(delta).toBeLessThanOrEqual(1);
     }
   });
+});
 
-  it("jpegjsEncoder & browserDecoder", async () => {
-    const n = 1024 * 1024;
-    const usedBitsN = 4;
-    const payload = randomBytesArray(n);
-
+describe("Check encoders & decoders", () => {
+  const n = 1024 * 1024;
+  const usedBitsN = 4;
+  const payload = randomBytesArray(n);
+  
+  it("jpegjsEncoder & jpegjsDecoder", async () => {
     const enc = new WeiboJpegEncoder(usedBitsN);
-    const dec = new WeiboJpegDecoder(usedBitsN);
-
     const encoded = await enc.Write(payload.buffer);
 
-    // const resp = await fetch(IMAGE_FILE.url);
-    // const encoded = await resp.arrayBuffer();
- 
-    // performance.mark("all");
+    const dec = new WeiboJpegDecoder(usedBitsN, WeiboJpegDecoder.jpegjsDecoder);
     const decoded = await dec.Read(encoded, n);
-    // console.log("all", performance.measure("all", "all").duration);
     expect(new Uint8Array(decoded)).toEqual(payload);
+  });
 
-    // console.log("decode", performance.getEntriesByName("browserDecoder:jpeg decode")[0].duration)
-    // console.log("canvas", performance.getEntriesByName("browserDecoder:jpeg canvas")[0].duration)
+  it("jpegjsEncoder & browserDecoder", async () => {
+    const enc = new WeiboJpegEncoder(usedBitsN);
+    const encoded = await enc.Write(payload.buffer);
+
+    const dec = new WeiboJpegDecoder(usedBitsN, WeiboJpegDecoder.browserDecoder);
+    const decoded = await dec.Read(encoded, n);
+    expect(new Uint8Array(decoded)).toEqual(payload);
   });
 });
