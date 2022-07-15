072e53fdf068e33d3965052ddd067f3892f9ceb5
Cong Zuo <zckevinzc@gmail.com>
2022-07-15 10:34:55 +0800

    Code format

--- src/config.js
+++ src/config.js
@@ -2,17 +2,17 @@ import { assert } from "./assert.js";
 import { UsedBits } from "./bits-manipulation.js";
 
 export class CipherConfig {
-	/**
-	 * @param {string} algorithm 
-	 * @param {Buffer} key 
-	 * @param {Buffer} iv 
-	 */
-	constructor(algorithm, key, iv) {
+  /**
+   * @param {string} algorithm 
+   * @param {Buffer} key 
+   * @param {Buffer} iv 
+   */
+  constructor(algorithm, key, iv) {
     assert(algorithm === "aes-128-gcm");
-		this.algorithm = algorithm;
-		this.key = key;
-		this.iv = iv;
-	}
+    this.algorithm = algorithm;
+    this.key = key;
+    this.iv = iv;
+  }
 }
 
 export class SinkUploadConfig {
@@ -24,14 +24,14 @@ export class SinkUploadConfig {
    * @param {symbol} encoder
    * @param {Number} sinkType
    */
-	constructor(usedBits, cipherConfig, validate, maskPhotoFilePath, encoder, sinkType) {
+  constructor(usedBits, cipherConfig, validate, maskPhotoFilePath, encoder, sinkType) {
     this.usedBits = usedBits;
     this.cipherConfig = cipherConfig;
     this.validate = validate;
     this.maskPhotoFilePath = maskPhotoFilePath;
     this.encoder = encoder;
     this.sinkType = sinkType;
-	}
+  }
 }
 
 export class SinkDownloadConfig {
@@ -40,11 +40,11 @@ export class SinkDownloadConfig {
    * @param {CipherConfig} cipherConfig 
    * @param {symbol} decoder 
    */
-	constructor(usedBits, cipherConfig, decoder) {
+  constructor(usedBits, cipherConfig, decoder) {
     this.usedBits = usedBits;
     this.cipherConfig = cipherConfig;
     this.decoder = decoder;
-	}
+  }
 
   cloneWithNewUsedBits(usedBits) {
     return new SinkDownloadConfig(usedBits, this.cipherConfig, this.decoder);
